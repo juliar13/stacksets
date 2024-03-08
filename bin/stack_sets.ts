69a4575ee65e940ec3009e99b866ec8520e56c39
sho-yamasaki <judydoctrine@gmail.com>
2024-03-08 20:51:39 +0900

    test

--- bin/stack_sets.ts
+++ bin/stack_sets.ts
@@ -7,8 +7,8 @@ import { CloudtrailStack } from '../lib/template/cloudtrail-stack';
 const app = new cdk.App();
 const stage = new cdk.Stage(app, 'template');
 const env = {
-  account: process.env.CDK_DEFAULT_ACCOUNT, // "851725447156",
-  region: process.env.CDK_DEFAULT_REGION    // "ap-northeast-1"
+  account: process.env.CDK_DEFAULT_ACCOUNT,
+  region: process.env.CDK_DEFAULT_REGION
 };
 
 new CloudtrailStack(stage, 'CloudTrailStack', {
@@ -16,22 +16,7 @@ new CloudtrailStack(stage, 'CloudTrailStack', {
 });
 const template1 = stage.synth().stacks[0].template;
 
-// const app = new cdk.App();
 new StackSetsStack(app, 'StackSetsStack', {
-  /* If you don't specify 'env', this stack will be environment-agnostic.
-   * Account/Region-dependent features and context lookups will not work,
-   * but a single synthesized template can be deployed anywhere. */
-
-  /* Uncomment the next line to specialize this stack for the AWS Account
-   * and Region that are implied by the current CLI configuration. */
-  // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
-
-  /* Uncomment the next line if you know exactly what Account and Region you
-   * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
-
-  /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
-
   stackSetsName: 'cloudtrail-stack',
   env: env,
   organizationalUnitIds: ['ou-213w-190ou3ha'], // Sandbox OU
